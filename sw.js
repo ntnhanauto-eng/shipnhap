23360bed4936380c1ae355bcc9d756b64b738dbc
ntnhanauto-eng <ntnhan.auto@gmail.com>
2026-04-18 11:02:22 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,18 +1,7 @@
-const CACHE_NAME = 'smart-system-v1';
-const urlsToCache = [
-  '/',
-  'index.html',
-  // Thêm các file css hoặc ảnh icon của bạn vào đây
-];
-
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
-  );
+self.addEventListener('install', e => {
+  console.log('Service Worker installed');
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
-  );
+self.addEventListener('fetch', e => {
+  // có thể cache sau
 });
